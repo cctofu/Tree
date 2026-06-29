b79b5671905ccb93e59a4d3536c8238d55aff931
cctofu <calvinchang216@gmail.com>
2026-06-28 21:47:38 -0700

    Initial Commit

---
+++ Tree/TreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeApp.swift
+//  Tree
+//
+//  Created by Calvin Chang on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TreeTests/TreeTests.swift
@@ -0,0 +1,16 @@
+//
+//  TreeTests.swift
+//  TreeTests
+//
+//  Created by Calvin Chang on 6/28/26.
+//
+
+import Testing
+
+struct TreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
